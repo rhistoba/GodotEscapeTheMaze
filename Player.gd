a4e665e4c80c5d4d3007a4aca04a052ed2054fa0
rhistoba <fujitakeisound@gmail.com>
2021-07-03 19:53:34 +0900

    add signal to player

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends "res://Character.gd"
 
 signal moved
+signal dead
+signal grabbed_key
+signal win
 
 func _process(delta):
     if can_move:
@@ -8,3 +11,13 @@ func _process(delta):
             if Input.is_action_pressed(dir):
                 if move(dir):
                     emit_signal('moved')
+
+func _on_Player_area_entered(area):
+    if area.is_in_group('enemies'):
+        emit_signal('dead')
+    if area.ha_method('pickup'):
+        area.pickup()
+        if area.type == 'key_red':
+            emit_signal('grabbed_key')
+        if area.type == 'star':
+            emit_signal('win')
