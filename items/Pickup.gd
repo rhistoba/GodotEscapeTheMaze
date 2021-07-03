fbb5e1b0541f9c6d9909972176a57a47e322f0f2
rhistoba <fujitakeisound@gmail.com>
2021-07-03 21:34:21 +0900

    add pickup scene

---
+++ items/Pickup.gd
@@ -0,0 +1,40 @@
+extends Area2D
+
+var textures = {
+    'coin': 'res://assets/coin.png',
+    'key_red': 'res://assets/keyRed.png',
+    'star': 'res://assets/star.png'
+}
+var type
+
+func _ready():
+    $Tween.interpolate_property(
+        $Sprite,
+        'scale',
+        Vector2(1, 1),
+        Vector2(3, 3),
+        0.5,
+        Tween.TRANS_QUAD,
+        Tween.EASE_IN_OUT
+    )
+    $Tween.interpolate_property(
+        $Sprite,
+        'modulate',
+        Color(1, 1, 1, 1),
+        Color(1, 1, 1, 0),
+        0.5,
+        Tween.TRANS_QUAD,
+        Tween.EASE_IN_OUT
+    )
+
+func _init(_type, pos):
+    $Sprite.texture = load(textures[_type])
+    type = _type
+    position = pos
+
+func pickup():
+    $CollisionShare2D.disabled = true
+    $Tween.start()
+
+func _on_Tween_tween_completed(object, key):
+    queue_free()
