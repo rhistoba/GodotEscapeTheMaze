37da266b9e77dee6b98c8223ed683c53255bfbea
rhistoba <fujitakeisound@gmail.com>
2021-07-04 21:12:33 +0900

    add player die animation

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -14,6 +14,12 @@ func _process(delta):
 
 func _on_Player_area_entered(area):
 	if area.is_in_group('enemies'):
+		var enemy = area
+		enemy.hide()
+		set_process(false)
+		$CollisionShape2D.set_deferred('disabled', true)
+		$AnimationPlayer.play('die')
+		yield($AnimationPlayer, 'animation_finished')
 		emit_signal('dead')
 	if area.has_method('pickup'):
 		area.pickup()
