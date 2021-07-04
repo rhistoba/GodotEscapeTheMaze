62437440117c19d15ceaa2b104d0fbf276ed218e
rhistoba <fujitakeisound@gmail.com>
2021-07-04 20:53:11 +0900

    add highscore

--- Global.gd
+++ Global.gd
@@ -10,13 +10,28 @@ var score = 0
 var start_screen = 'res://ui/StartScreen.tscn'
 var end_screen = 'res://ui/EndScreen.tscn'
 
+var highscore = 0
+var score_file = 'user://highscore.txt'
+
+func _ready():
+	setup()
+
 func new_game():
 	score = 0
 	current_level = -1
 	next_level()
 
 func game_over():
+	if score > highscore:
+		highscore = score
+		save_score()
 	get_tree().change_scene(end_screen)
+
+func save_score():
+	var f = File.new()
+	f.open(score_file, File.WRITE)
+	f.store_string(str(highscore))
+	f.close()
 
 func next_level():
 	current_level += 1
@@ -24,3 +39,11 @@ func next_level():
 		game_over()
 	else:
 		get_tree().change_scene(levels[current_level])
+
+func setup():
+	var f = File.new()
+	if f.file_exists(score_file):
+		f.open(score_file)
+		var content = f.get_as_text()
+		highscore = int(content)
+		f.close()
--- ui/StartScreen.gd
+++ ui/StartScreen.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+	$ScoreNotice.text = 'Highscore: ' + str(Global.highscore)
+
 func _input(event):
-    if event.is_action_pressed('ui_select'):
-        Global.new_game()
+	if event.is_action_pressed('ui_select'):
+		Global.new_game()
